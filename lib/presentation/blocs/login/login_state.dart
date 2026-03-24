f75ebb6d6607eccfab10b0ed24c0bbaa5f381bb0
Emmanuel-NS <e.nsabagasa@alustudent.com>
2026-03-24 19:54:12 +0200

    feat(auth): Implement Login Cubit for detailed state management

--- lib/presentation/blocs/login/login_state.dart
+++ lib/presentation/blocs/login/login_state.dart
@@ -6,11 +6,13 @@ class LoginState extends Equatable {
   const LoginState({
     this.status = LoginStatus.initial,
     this.errorMessage,
+    this.userEmail,
   });
 
   final LoginStatus status;
   final String? errorMessage;
+  final String? userEmail;
 
   @override
-  List<Object?> get props => [status, errorMessage];
+  List<Object?> get props => [status, errorMessage, userEmail];
 }
